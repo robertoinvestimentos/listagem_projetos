c22b5670d3a5ce95d09b04178d605717b097472a
André Costa <andresilvacosta1992@gmail.com>
2024-06-23 20:43:51 -0300

    commit automático

--- rastreador/24/06/modulos/main.js
+++ rastreador/24/06/modulos/main.js
@@ -1,12 +1,19 @@
 import { dados } from './data.js';
 
+function getDiaSemana(dia){
+    const diasSemana = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'];
+    const data = new Date(`2024-06-${dia}`);
+    return diasSemana[data.getDay()];
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const tableBody = document.getElementById('tableBody');
 
     Object.entries(dados).forEach(([dia, tempo]) => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${dia < 10 ? '0' + dia : dia}/06/24</td>
+            <td>${dia < 10 ? '0' + dia : dia}/06/24 (${getDiaSemana(dia)})</td>
             <td>${tempo !== null ? tempo : '-'}</td>
         `;
         tableBody.appendChild(row);
