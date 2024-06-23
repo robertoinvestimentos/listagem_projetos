973e4706de7814b87d4948335401119fc2b84b88
André Costa <andresilvacosta1992@gmail.com>
2024-06-23 18:12:07 -0300

    commit automático

--- script.js
+++ script.js
@@ -48,7 +48,15 @@ const projects = [
         "completion": 45, // Porcentagem de conclusão
         "link": "https://publicidade-recompensa.netlify.app/",
         "image": "/assets/imgs/projetos/publicidade_recompensa.jpg"
-    }
+    },
+    {
+        "title": "Sistema de Atualização de Resultados de Loterias",
+        "description": "O <strong>Sistema de Atualização de Resultados de Loterias</strong> é uma plataforma desenvolvida para automatizar o processo de atualização das informações de resultados de loterias no site da LotoDicas. Com esse sistema, é possível manter os dados sempre atualizados em tempo real, garantindo que os usuários tenham acesso às informações mais recentes sobre os sorteios das loterias. Além disso, o sistema oferece recursos de agendamento e notificação para facilitar a gestão e manutenção dos resultados.",
+        "completion": 60, // Porcentagem de conclusão
+        "link": "https://atualizacao-loterias.netlify.app/",
+        "image": "/assets/imgs/projetos/atualizacao_loterias.jpg"
+    },
+    
     
     
 
